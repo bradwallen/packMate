9e99db580f299d8455c5bdddc4dbafc728e50e06
Brad W. Allen <ballen12@gmail.com>
2015-04-22 21:25:21 -0500

    I added the same code to the GearViewController and added some test gear items.

--- packMate/GearViewController.swift
+++ packMate/GearViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class GearViewController: UIViewController {
-
+class GearViewController: UIViewController, UITableViewDataSource {
+    
+    var userGear = [
+        Gear(name: "knife"),
+        Gear(name: "sleeping bag"),
+        Gear(name: "canteen"),
+        Gear(name: "flint"),
+        Gear(name: "tent"),
+        Gear(name: "headlamp"),
+        Gear(name: "small dry bag"),
+        Gear(name: "medium dry bag"),
+        Gear(name: "tent stakes"),
+        Gear(name: "AAA batteries"),
+        Gear(name: "socks"),
+        Gear(name: "water filter") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userGear.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let gear : Gear = userGear[indexPath.row]
+        
+        cell.textLabel?.text = gear.name
+        
+        return cell
+    }
+    
 }
-
